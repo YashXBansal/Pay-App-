1fe978816ad7b4c10b815d77c6a71f2a05728189
YashXBansal <yashbansal414@gmail.com>
2024-02-23 16:37:43 +0530

    improving backend

--- SERVER/index.js
+++ SERVER/index.js
@@ -3,14 +3,22 @@ const mainRouter = require("./ROUTES/index");
 const app = express();
 const dotenv = require("dotenv");
 const { mongoConnection } = require("./DATABASE/db");
+const { User, Account } = require("./DATABASE/db");
 const cors = require("cors");
 dotenv.config();
 const PORT = process.env.PORT;
+
+// Establish MongoDB connection
 mongoConnection();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use("/api/v1", mainRouter);
 
+// Start the server
 app.listen(PORT, () => {
   console.log("Server is running at", PORT);
 });
